06adff4cb67e74027174cfde868a32114b533eb7
belayb <belaybirlie.yimer@manchester.ac.uk>
2021-12-02 21:17:15 +0000

    day 2

--- plainR/solution.R
+++ plainR/solution.R
@@ -3,4 +3,7 @@ here::i_am("plainR/solution.R")
 measurement<-as.integer(readLines(here("data/measurement_day1.txt")))
 sum(diff(measurement)>0)
 
-
+library(data.table)
+day2<-readLines(here("data/day2.txt"))
+day2<-setDT(day2)[, paste0("v", 1:2) := tstrsplit(day2, " ")]
+(sum(day2$v2[day2$v1 == "down"]) - sum(day2$v2[day2$v1 == "up"]))*sum(day2$v2[day2$v1 == "forward"])
