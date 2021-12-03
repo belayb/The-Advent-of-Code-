5f1cb0184ad75b3867dbdf8496a7cffc17c5dd5f
belayb <belaybirlie.yimer@manchester.ac.uk>
2021-12-03 09:34:05 +0000

    day2

--- plainR/solution.R
+++ plainR/solution.R
@@ -5,5 +5,8 @@ sum(diff(measurement)>0)
 
 library(data.table)
 day2<-readLines(here("data/day2.txt"))
-day2<-setDT(day2)[, paste0("v", 1:2) := tstrsplit(day2, " ")]
-(sum(day2$v2[day2$v1 == "down"]) - sum(day2$v2[day2$v1 == "up"]))*sum(day2$v2[day2$v1 == "forward"])
+day2<-setDT(as.data.frame(day2))[, paste0("v", 1:2) := tstrsplit(day2, " ")]
+day2<-as.data.frame(day2[, .(sum.depth = sum(as.numeric(v2))), by = v1])
+(day2$sum.depth[day2$v1 == "down"] - day2$sum.depth[day2$v1 == "up"])*day2$sum.depth[day2$v1 == "forward"]
+
+
