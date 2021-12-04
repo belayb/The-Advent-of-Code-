270b47f92af0f78d2652d9141d13b6f6e327f040
belayb <belaybirlie.yimer@manchester.ac.uk>
2021-12-04 20:32:02 +0000

    day 3 part 1

--- plainR/solution.R
+++ plainR/solution.R
@@ -9,3 +9,12 @@ day2<-as.data.frame(setDT(as.data.frame(day2))[, paste0("v", 1:2) := tstrsplit(d
 (day2$sum.depth[day2$v1 == "down"] - day2$sum.depth[day2$v1 == "up"])*day2$sum.depth[day2$v1 == "forward"]
 
 
+
+day3<-readLines(here("data/day3.txt"))
+day3<-do.call( "rbind", strsplit(day3, split = ""))
+day3 <- matrix(as.numeric(day3), ncol = 12)
+strtoi(paste(ifelse(colSums(day3)/nrow(day3) >0.5, 1,0),collapse=""),base=2)*
+  strtoi(paste(abs(ifelse(colSums(day3)/nrow(day3) >0.5, 1,0)-1),collapse=""),base=2)
+  
+
+
